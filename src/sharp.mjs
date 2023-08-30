d7b7d026fd5b690ff769538820863bce49656d1f
salles-g <salles.gustavog@gmail.com>
2023-08-30 15:44:28 -0300

    chore: move to .mjs and use "fill" instead of "contain"

--- src/sharp.mjs
+++ src/sharp.mjs
@@ -1,18 +1,23 @@
-const sharp = require("sharp");
+import fs from "fs";
+import sharp from "sharp";
 
 const temp = "dist/temp.png";
 const path = (name) => `./images/${name}`;
 const image = (name) => sharp(path(name));
+const soyMap = {
+  point: "soyjak-pointing.png",
+};
 
 /**
  * Receives a background image and composites a soyjak pointing to it
  */
-async function compositeSoyjak(soyjak, source) {
+export async function compositeSoyjak(soyjak, source) {
+  soyjak = soyMap[soyjak];
   const background = image(source);
   const { width, height } = await background.metadata();
 
   const soyjakInst = image(soyjak).resize(width, height, {
-    fit: "contain",
+    fit: "fill",
   });
 
   await soyjakInst.toBuffer().then((input) => {
@@ -20,10 +25,11 @@ async function compositeSoyjak(soyjak, source) {
   });
 
   // Now we read the generated "temp" file and trim its borders
-  await sharp(temp).trim().toFile(`dist/${source}`);
+  await sharp(temp).trim().toFile(`dist/new.png`);
 
-  // And finally, we delete the temp file
-  require("fs").unlinkSync(temp);
+  // And finally, we delete the temp file and the source
+  fs.unlinkSync(temp);
+  fs.unlinkSync(path(source));
+
+  return;
 }
-
-compositeSoyjak("soyjak-pointing.png", "switch.jpg");
