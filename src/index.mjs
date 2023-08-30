4fd82194abaeaa7368fd943d70d923c3c88f3067
salles-g <salles.gustavog@gmail.com>
2023-08-30 18:04:12 -0300

    refactor: change to IIFE w/ a try/catch block

--- src/index.mjs
+++ src/index.mjs
@@ -2,6 +2,12 @@ import { listen } from "./discord/listen.mjs";
 import { setup } from "./discord/setup.mjs";
 import dotenv from "dotenv";
 
-dotenv.config();
-await setup();
-listen();
+(async () => {
+  try {
+    dotenv.config();
+    await setup();
+    listen();
+  } catch {
+    console.error("Failed to start bot");
+  }
+})();
