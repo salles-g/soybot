54163150f92f655b3a9a747c5d5051e187a4e11a
salles-g <salles.gustavog@gmail.com>
2023-08-30 16:47:08 -0300

    refactor: add "reply" utils

--- src/discord/utils/message.mjs
+++ src/discord/utils/message.mjs
@@ -18,3 +18,32 @@ export const messageInfo = (message) => {
 
   return { content, image, replied };
 };
+
+export const reply = async (message, content) => {
+  const { replied } = messageInfo(message);
+  // If the user is replying to a message, replied to that
+  // with "It's over"
+  if (replied.id) {
+    // Delete the original message
+    message.delete();
+    // And reply to the replied message
+    return await message.channel.messages
+      .fetch(replied.id)
+      .then((msg) => {
+        msg.reply(content);
+      });
+  }
+
+  // Else, reply to the original image
+  await message.reply(content);
+  return;
+};
+export const replyWithImage = async (message, image) => {
+  return await reply(message, {
+    files: [image],
+  });
+};
+
+export const replyWithText = async (message, text) => {
+  return await reply(message, text);
+};
